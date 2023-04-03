71b77cbf5676c48df20b87e976279726732b41bc
Mritunjay Agarwal <39786312+mritunjayagarwal@users.noreply.github.com>
2023-04-03 19:25:39 +0530

    Change

--- src/Home/socials.js
+++ src/Home/socials.js
@@ -1,16 +1,16 @@
 const socials = [
     {
         id: 1,
-        icon: '8.png',
-        url: 'http://www.twitter.com'
+        icon: '9.png',
+        url: 'https://twitter.com/leverageerc'
     },
     {
-        id: 1,
-        icon: '9.png',
+        id: 2,
+        icon: '8.png',
         url: 'https://t.me/leverageprotocol'
     },
     {
-        id: 1,
+        id: 3,
         icon: '10.png',
         url: 'http://www.behance.com'
     },
